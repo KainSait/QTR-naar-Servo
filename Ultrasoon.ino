d898142082e09a015a35585e5b797141945301f5
ROG-STRIX\gabcr <hallohallo.gs@gmail.com>
2022-07-08 12:25:48 +0100

    no message

--- Ultrasoon.ino
+++ Ultrasoon.ino
@@ -1,3 +1,7 @@
+#include <Servo.h>
+
+//--------------------------------------------------------------------------------------------------------------------------
+Servo esc;
 unsigned long PulseTime = 0;    
 int MicroswitchMEM = 0;  // Geheugen om de stand van de Microswitch te onthouden
 int Microswitch = 19;     // De microswitch zit aangesloten op PIN 2
@@ -7,20 +11,26 @@ int GREENPIN = 10;       // Groen zit aangesloten op PIN 10
 int RODEPIN = 11;        // Rood zit aangesloten op PIN 11
 const int trigPin = 37;  // Trig zit aangesloten op PIN 12
 const int echoPin = 38;  // Echo zit aangesloten op PIN 13
+const int Start_knop  = 8;
+int Start_MEM   = 0;
+
+
 
 long duration;  // Tijd in microsecondes gedetecteerd door de ultrasoon sensor
 int distance;   // De afstand in cm van de ultrasoon sensor tot het gedetecteerde voorwerp
 
 
 void setup() {
+  pinMode(Start_knop, INPUT);
   pinMode(Microswitch, INPUT_PULLUP);  // Veranderd de Microswitch naar een Input
   pinMode(BumperReset, INPUT);         // veranderd de BumperReset knop naar een Input
   pinMode(trigPin, OUTPUT);            // Veranderd de trigPin naar een Output
   pinMode(echoPin, INPUT);             // Veranderd de echoPin naar een Input
   Serial.begin(9600);                  // Starts the serial communication
+  esc.attach(12);
 }
 
-
+//--------------------------------------------------------------------------------------------------------------------------
 void loop() {
 
   // ULTRASOON GEDEELTE
@@ -38,12 +48,13 @@ void loop() {
   Serial.println(MicroswitchMEM);
 
 
-
+  //--------------------------------------------------------------------------------------------------------------------------
   // RESPONSE GEDEELTE
 
   if (digitalRead(Microswitch) == HIGH) {  // Schakelt de rode Leds in en de blauwe Leds uit als de gemeten afstand lager dan 30 is
     GeleLeds();
     MicroswitchMEM = 1;
+    Start_MEM = 0;
   } else if (MicroswitchMEM == 0) {
     GroeneLeds();
   }
@@ -51,6 +62,7 @@ void loop() {
   if (distance < 30) {  // Schakelt de rode Leds in en de blauwe Leds uit als de gemeten afstand lager dan 30 is
     GeleLeds();
     MicroswitchMEM = 1;
+    Start_MEM = 0;
 
     
   } else if (MicroswitchMEM == 0) {
@@ -61,12 +73,33 @@ void loop() {
     MicroswitchMEM = 0;
   }
 
+  //--------------------------------------------------------------------------------------------------------------------------
+  // Start/stop cyclus
+  
+  if (digitalRead(BumperReset) == HIGH) {
+  Start_MEM = 1;
+  }
+
+  // einde Start/stop cyclus
+  //--------------------------------------------------------------------------------------------------------------------------
+  // Motor aan stuur gedeelte
+
+  
+if (Start_MEM == 1 ) {
+  esc.writeMicroseconds(1770);
+  }
+else if (Start_MEM == 0  ) {
+  esc.writeMicroseconds(2400);
+  Serial.println(Start_MEM);
+  delay(250);
+  }
+
 
 
 
   // einde void loop
 }
-
+  //--------------------------------------------------------------------------------------------------------------------------
 
 
 void BlueLeds() {
@@ -75,7 +108,7 @@ void BlueLeds() {
   analogWrite(RODEPIN, 0);
 }
 void GroeneLeds() {
-  analogWrite(BLUEPIN, 0);  // Schakelt de blauwe Leds in en de rode Leds uit als de gemeten afstand niet lager dan 30 is
+  analogWrite(BLUEPIN, 0);  
   analogWrite(GREENPIN, 255);
   analogWrite(RODEPIN, 0);
 }
@@ -85,4 +118,6 @@ void GeleLeds () {
   analogWrite(GREENPIN, 50);
   analogWrite(BLUEPIN, 100);
 }
+
+
   
